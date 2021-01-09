33ae53e0b5d0e0374cc97f33211844f515c4bbe8
anurag.a <anuanchan22@gmail.com>
2021-01-09 15:55:30 +0530

    we added platfrom

--- sketch.js
+++ sketch.js
@@ -17,7 +17,7 @@ function setup(){
 
 
     ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
+     platform = new Ground(150,305,300,170)
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -40,7 +40,11 @@ function setup(){
 
 function draw(){
     background(backgroundImg);
-    Engine.update(engine);
+ 
+    Engine.update(engine); 
+    fill ("purple")  
+    text("Anurag",500,400)
+
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
     console.log(box2.body.angle);
@@ -61,4 +65,5 @@ function draw(){
 
     bird.display();
     platform.display();
+
 }
